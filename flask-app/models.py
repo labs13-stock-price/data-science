ae57e9e69f5ce627b22aa4c6aa260f3f2de4b261
lajawanti <myemail>
2019-05-31 17:01:25 -0400

    Still doing debug...

--- flask-app/models.py
+++ flask-app/models.py
@@ -5,4 +5,4 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
+    name = db.Column(db.String(100))
