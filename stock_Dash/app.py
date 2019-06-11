38320dc4024344be7cf99c2e618592fae68ed7f6
Dmitriy <dkav@live.com>
2019-06-11 09:49:51 -0400

    Dash free v 004

--- stock_Dash/app.py
+++ stock_Dash/app.py
@@ -1,17 +1,41 @@
 import dash
+from dash.dependencies import Output, Input, State
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
 
 import pandas_datareader.data as web
 from datetime import datetime
 import pandas as pd
 import requests
 import numpy as np
+import sqlite3
+from collections import Counter
+import string
+import regex as re
+import time
+import pickle
+import nltk
+import re
+import sys
+import os
+import json
+import base64
+
+sys.path.insert(0, os.path.realpath(os.path.dirname(__file__)))
+os.chdir(os.path.realpath(os.path.dirname(__file__)))
+
+from cache import cache
+from config import stop_words
 
 
 app = dash.Dash(__name__)
 server = app.server
+
+conn = sqlite3.connect('twitter_wloc.db', check_same_thread=False)
+
 
 # Get list of company names and symbols from csv
 nsdq = pd.read_csv('companylist.csv')
@@ -21,79 +45,58 @@ for tic in nsdq.index:
     options.append({'label':'{} {}'.format(tic,nsdq.loc[tic]['Name']),
                     'value':tic})
 
-xs = np.linspace(0, 20, 100)
-ys = np.sin(xs) + xs*0.1
-df = pd.DataFrame({'x': xs, 'y': ys})
-
 app.layout = html.Div([
-    html.H1('Stock Market Analytics'),
-    html.Div([
-        html.H3('Select ticker symbols:', style={'paddingRight':'30px'}),
-        dcc.Dropdown(
-            id='my_ticker_symbol',
-            options=options,
-            value=['SPY'],
-            multi=True
-        )
-    ], style={'display':'inline-block', 'verticalAlign':'top', 'width':'30%'}),
-    html.Div([
-        html.H3('Select start and end dates:'),
-        dcc.DatePickerRange(
-            id='my_date_picker',
-            min_date_allowed=datetime(2015, 1, 1),
-            max_date_allowed=datetime.today(),
-            start_date=datetime(2018, 1, 1),
-            end_date=datetime.today()
-        )
-    ], style={'display':'inline-block'}),
-    html.Div([
-        html.Button(
-            id='submit-button',
-            n_clicks=0,
-            children='Submit',
-            style={'fontSize':20, 'marginLeft':'20px'}
-        ),
-    ], style={'display':'inline-block'}),
-    dcc.Graph(
-        id='stock-price',
-        figure={
-            'data': [
-                {'x': [1,2], 'y': [3,1]}
-            ]
-        }
-    ),
-    dcc.Graph(
-        id='stock-sentiment',
-        figure={
-            'data': [
-                {'x': [1,2], 'y': [3,1]}
-            ]
-        }
-    ),
-    dcc.Graph(
-        id='stock-returns',
-        figure={
-            'data': [
-                {'x': [1,2], 'y': [3,1]}
-            ]
-        }
-    ),
-    # # Graph for testing
-    # dcc.Graph(
-    #     id='xyz',
-    #     figure={
-    #         'data': [
-    #             {'x': xs, 'y': df.y, 'line':{'color':'black'}},
-    #             {'x': xs, 'y': df.y.where(df.y <= 1), 'line':{'color':'green'}}
-    #         ]
-    #     }
-    # )
+
+    html.Div([html.H3('Select ticker symbols:'),
+             dcc.Dropdown(id='my_ticker_symbol',options=options,value=['TWTR'],
+                          multi=True)]),
+
+    html.Div([html.H3('Select start and end dates:'),
+             dcc.DatePickerRange(id='my_date_picker',
+                                 min_date_allowed=datetime(2015, 1, 1),
+                                 max_date_allowed=datetime.today(),
+                                 start_date=datetime(2018, 1, 1),
+                                 end_date=datetime.today())]),
+
+    html.Div([html.Button(id='submit-button1',n_clicks=0,children='Submit')]),
+
+    html.Div(className='row', children=[
+
+             html.Div(dcc.Graph(id='stock-price',
+                                figure={'data': [{'x': [1,2], 'y': [3,1]}]}),
+                                className='col s12 m6 l6'),
+
+            html.Div(dcc.Graph(id='risk-profile',
+                               figure={'data': [{'x': [1,2], 'y': [3,1]}]}),
+                               className='col s12 m6 l6')]),
+
+    html.Div(className='row', children=[
+
+            html.Div(dcc.Graph(id='stock-sentiment',
+                               figure={'data': [{'x': [1,2], 'y': [3,1]}]}),
+                               className='col s12 m6 l4'),
+
+            html.Div([dcc.Input(id='sentiment_term', value='twitter', type='text'),
+                      html.Div(['example'], id='input-div', style={'display': 'none'}),
+                      html.Button('submit', id="submit-button"),
+                      dcc.Graph(id='live-graph',animate=False)],
+                      className='col s12 m6 l4'),
+
+            html.Div(dcc.Graph(id='sentiment-pie', animate=False),
+                     className='col s12 m6 l4')]),
+
+    dcc.Interval(id='live-graph-update',interval=5*1000, n_intervals=0,
+                 max_intervals=20),
+
+    dcc.Interval(id='sentiment-pie-update',interval=5*1000, n_intervals=0,
+                 max_intervals=20),
+
 ])
 
 
 """This callback function returns the stock price chart with candlesticks"""
 @app.callback(Output('stock-price', 'figure'),
-             [Input('submit-button', 'n_clicks')],
+             [Input('submit-button1', 'n_clicks')],
              [State('my_ticker_symbol', 'value'),
               State('my_date_picker', 'start_date'),
               State('my_date_picker', 'end_date')])
@@ -108,7 +111,7 @@ def update_graph(n_clicks, stock_ticker, start_date, end_date):
         # Append each trace to a list so it can be called from n_click
         traces.append({'open':df.open, 'high':df.high, 'low':df.low,
                        'close':df.close, 'x':df.index, 'y':df.close,
-                       'type':'candlestick', 'name':tic})
+                       'type':'ohlc', 'name':tic})
     fig = {
         'data': traces,
         'layout': {'title':', '.join(stock_ticker)+' Price Chart',
@@ -116,10 +119,9 @@ def update_graph(n_clicks, stock_ticker, start_date, end_date):
     }
     return fig
 
-"""Call back function returns the stock sentiment
-   chart from Twitter sentiment"""
+"""This call back function returns the overall sentiment from Twitter"""
 @app.callback(Output('stock-sentiment', 'figure'),
-             [Input('submit-button', 'n_clicks')],
+             [Input('submit-button1', 'n_clicks')],
              [State('my_ticker_symbol', 'value'),
               State('my_date_picker', 'start_date'),
               State('my_date_picker', 'end_date')])
@@ -153,16 +155,16 @@ def update_graph(n_clicks, stock_ticker, start_date, end_date):
                        'line':{'color':'red'}, 'name':'negative sentiment'})
     fig = {
         'data':traces,
-        'layout':{'title':', '.join(stock_ticker)+' Twitter Sentiment Index',
+        'layout':{'title':', '.join(stock_ticker)+' Overall Sentiment Index',
         'showlegend':False}
     }
 
     return fig
 
 """This callback function returns the distributon
-   of daily returns on a dist to give risk profile"""
-@app.callback(Output('stock-returns', 'figure'),
-             [Input('submit-button', 'n_clicks')],
+   of daily returns on a dist plot to give risk profile"""
+@app.callback(Output('risk-profile', 'figure'),
+             [Input('submit-button1', 'n_clicks')],
              [State('my_ticker_symbol', 'value'),
               State('my_date_picker', 'start_date'),
               State('my_date_picker', 'end_date')])
@@ -190,6 +192,128 @@ def update_graph(n_clicks, stock_ticker, start_date, end_date):
     }
     return fig
 
+def removeSpecialChar (raw_text):
+    for k in raw_text.split("\n"):
+        # Remove all special characters
+        return(re.sub(r"[^a-zA-Z0-9]+", ' ', k))
+
+MAX_DF_LENGTH = 100
+
+def df_resample_sizes(df, maxlen=MAX_DF_LENGTH):
+    df_len = len(df)
+    resample_amt = 100
+    vol_df = df.copy()
+    vol_df['volume'] = 1
+
+    ms_span = (df.index[-1] - df.index[0]).seconds * 1000
+    rs = int(ms_span / maxlen)
+
+    df = df.resample('{}ms'.format(int(rs))).mean()
+    df.dropna(inplace=True)
+
+    vol_df = vol_df.resample('{}ms'.format(int(rs))).sum()
+    vol_df.dropna(inplace=True)
+
+    df = df.join(vol_df['volume'])
+
+    return df
+
+
+"""This callback updates the live sentiment and pie chart div on button click"
+   when the interval triggers or when the div changes"""
+@app.callback(Output('input-div', 'children'),
+             [Input('submit-button', 'n_clicks')],
+              state=[State(component_id='sentiment_term', component_property='value')])
+def update_div(n_clicks, sentiment_term):
+    sentiment_term = removeSpecialChar(sentiment_term)
+    return sentiment_term
+
+"""This callback takes in data from the input-div and graph update placeholder
+   to update the live sentiment graph"""
+@app.callback(Output('live-graph', 'figure'),
+              [Input('input-div', 'children'),
+               Input('live-graph-update', 'n_intervals')])
+def update_graph_scatter(sentiment_term, n_intervals):
+
+    t17 = time.time()
+
+    try:
+
+        if sentiment_term:
+            sql_term = "SELECT unix, sentiment FROM sentiment WHERE tweet LIKE '%"+sentiment_term+"%' ORDER BY id DESC LIMIT 800"
+            df = pd.read_sql(sql_term, conn)
+        else:
+            df = pd.read_sql("SELECT * FROM sentiment ORDER BY id DESC, unix DESC LIMIT 800", conn)
+
+        df.sort_values('unix', inplace=True)
+        df['date'] = pd.to_datetime(df['unix'], unit='ms')
+        df.set_index('date', inplace=True)
+        init_length = len(df)
+        df['sentiment_smoothed'] = df['sentiment'].rolling(int(len(df)/5)).mean()
+        df = df_resample_sizes(df)
+        X = df.index
+        Y = df.sentiment_smoothed.values
+        Y2 = df.volume.values
+        data = plotly.graph_objs.Scatter(
+                x=X,
+                y=Y,
+                name='Sentiment',
+                mode= 'lines',
+                yaxis='y2')
+
+        data2 = plotly.graph_objs.Bar(
+                x=X,
+                y=Y2,
+                name='Volume')
+
+        t27 = time.time()
+        print('live graph app call back bitirdim: "{}" '.format(sentiment_term), t27 - t17)
+
+        return {'data': [data,data2],'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),
+                                                          yaxis=dict(range=[min(Y2),max(Y2*4)], title='Volume', side='right'),
+                                                          yaxis2=dict(range=[min(Y),max(Y)], side='left', overlaying='y',title='sentiment'),
+                                                          title='Live sentiment for: "{}"'.format(sentiment_term),
+                                                          showlegend=False)}
+
+    except Exception as e:
+        with open('errors.txt','a') as f:
+            f.write(str(e))
+            f.write('\n')
+
+
+
+"""This callback outputs the live pie chart"""
+@app.callback(Output('sentiment-pie', 'figure'),
+              [Input('input-div', 'children'),
+               Input('sentiment-pie-update', 'n_intervals')])
+def update_pie_chart(sentiment_term, n_intervals):
+    t16 = time.time()
+
+    sql_term = "SELECT COUNT(tweet) AS POS FROM sentiment WHERE tweet LIKE '%"+sentiment_term+"%' AND sentiment > 0.1"
+    df = pd.read_sql(sql_term, conn)
+    pos= df['POS'][0]
+    sql_term = "SELECT COUNT(tweet) AS NEG FROM sentiment WHERE tweet LIKE '%"+sentiment_term+"%' AND sentiment < - 0.1"
+    df = pd.read_sql(sql_term, conn)
+    neg= df['NEG'][0]
+    labels = ['Positive tweets','Negative tweets']
+    values = [pos,neg]
+    colors = ['green', 'red']
+
+    trace = go.Pie(labels=labels, values=values,
+                   hoverinfo='label+percent', textinfo='value',
+                   marker=dict(colors=colors,line=dict(width=2)))
+
+    t26 = time.time()
+    print('setiment pie app call back bitirdim:  "{}" '.format(sentiment_term), t26 - t16)
+
+    return {"data":[trace],'layout' : go.Layout(title=''.format(sentiment_term),
+                                                showlegend=True)}
+
+
+external_css = ["materialize.css"]
+for css in external_css:
+    app.css.append_css({"external_url": css})
+
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, port=8070) # don't forget to change the port before deploying
---
+++ stock_Dash/twitter.py
@@ -0,0 +1,288 @@
+import os
+import sys
+sys.path.insert(0, os.path.realpath(os.path.dirname(__file__)))
+os.chdir(os.path.realpath(os.path.dirname(__file__)))
+
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+import sqlite3
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from unidecode import unidecode
+import time
+from threading import Lock, Timer
+import pandas as pd
+from config import stop_words
+import regex as re
+from collections import Counter
+import string
+import pickle
+import itertools
+from textblob import TextBlob
+import datetime
+
+analyzer = SentimentIntensityAnalyzer()
+
+# Twitter keys
+
+"""Isolation lever disables automatic transactions, we are disabling thread
+   check as we are creating connection here, but we'll be inserting from a
+   separate thread (no need for serialization)"""
+
+conn = sqlite3.connect('twitter_wloc.db', isolation_level=None, check_same_thread=False)
+c = conn.cursor()
+
+
+
+us_state_abbrev = {
+        'AK': 'Alaska',
+        'AL': 'Alabama',
+        'AR': 'Arkansas',
+        'AZ': 'Arizona',
+        'CA': 'California',
+        'CO': 'Colorado',
+        'CT': 'Connecticut',
+        'DC': 'District of Columbia',
+        'DE': 'Delaware',
+        'FL': 'Florida',
+        'GA': 'Georgia',
+        'HI': 'Hawaii',
+        'IA': 'Iowa',
+        'ID': 'Idaho',
+        'IL': 'Illinois',
+        'IN': 'Indiana',
+        'KS': 'Kansas',
+        'KY': 'Kentucky',
+        'LA': 'Louisiana',
+        'MA': 'Massachusetts',
+        'MD': 'Maryland',
+        'ME': 'Maine',
+        'MI': 'Michigan',
+        'MN': 'Minnesota',
+        'MO': 'Missouri',
+        'MS': 'Mississippi',
+        'MT': 'Montana',
+        'NC': 'North Carolina',
+        'ND': 'North Dakota',
+        'NE': 'Nebraska',
+        'NH': 'New Hampshire',
+        'NJ': 'New Jersey',
+        'NM': 'New Mexico',
+        'NV': 'Nevada',
+        'NY': 'New York',
+        'OH': 'Ohio',
+        'OK': 'Oklahoma',
+        'OR': 'Oregon',
+        'PA': 'Pennsylvania',
+        'RI': 'Rhode Island',
+        'SC': 'South Carolina',
+        'SD': 'South Dakota',
+        'TN': 'Tennessee',
+        'TX': 'Texas',
+        'UT': 'Utah',
+        'VA': 'Virginia',
+        'VT': 'Vermont',
+        'WA': 'Washington',
+        'WI': 'Wisconsin',
+        'WV': 'West Virginia',
+        'WY': 'Wyoming'
+}
+
+#search the location string for a long form state name. If found, abbreviate into state code.
+def update_state (loc, us_state_abbrev):
+    state_abbrev = ''
+    for key, value in us_state_abbrev.items():
+        #print (value)
+        if value.upper() in loc.upper():
+            state_abbrev = key
+            #print ('new state code is: ', state_abbrev)
+            return state_abbrev
+
+def create_table():
+    try:
+        # http://www.sqlite.org/pragma.html#pragma_journal_mode
+        # for us - it allows concurrent write and reads
+        c.execute("PRAGMA journal_mode=wal")
+        c.execute("PRAGMA wal_checkpoint=TRUNCATE")
+        c.execute("PRAGMA auto_vacuum = INCREMENTAL")
+        c.execute("PRAGMA main.incremental_vacuum")
+
+        # changed unix to INTEGER (it is integer, sqlite can use up to 8-byte long integers)
+        c.execute("CREATE TABLE IF NOT EXISTS sentiment(id INTEGER PRIMARY KEY AUTOINCREMENT, unix INTEGER, tweet TEXT, loc VARCHAR, state_code VARCHAR, city_name TEXT, country TEXT, followers_count INTEGER, sentiment REAL)")
+        # key-value table for random stuff
+        c.execute("CREATE TABLE IF NOT EXISTS misc(key TEXT PRIMARY KEY, value TEXT)")
+        # US states sentiment summary tabe
+        c.execute("CREATE TABLE IF NOT EXISTS states(state_code VARCHAR PRIMARY KEY, sentiment REAL, nb_tweets INTEGER)")
+        # id on index, both as DESC (as you are sorting in DESC order)
+        c.execute("CREATE INDEX id_unix ON sentiment (id DESC, unix DESC)")
+        # out full-text search table, i choosed creating data from external (content) table - sentiment
+        # instead of directly inserting to that table, as we are saving more data than just text
+        # https://sqlite.org/fts5.html - 4.4.2
+        #c.execute("CREATE VIRTUAL TABLE sentiment_fts USING fts5(tweet, content=sentiment, content_rowid=id, prefix=1, prefix=2, prefix=3)")
+        # that trigger will automagically update out table when row is inserted
+        # (requires additional triggers on update and delete)
+        #c.execute(
+        #    CREATE TRIGGER sentiment_insert AFTER INSERT ON sentiment BEGIN
+        #        INSERT INTO sentiment_fts(rowid, tweet) VALUES (new.id, new.tweet);
+        #    END
+        #        )
+    except Exception as e:
+        print(str(e))
+create_table()
+
+# create lock
+lock = Lock()
+
+# states  source : https://github.com/KobaKhit/sixopy/blob/master/sixopy/tw.py
+states = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY']
+states = [x for x in map(lambda x:x.upper(),states)]
+#state and city name splitter.  source : https://github.com/KobaKhit/sixopy/blob/master/sixopy/tw.py
+def get_state_city(loc):
+  # extracts state and city if state is a US state
+  # Args:
+  #  - location - string (Phoenix, AZ)
+  statecity = ['','']
+  if loc != None and ',' in loc:
+    loc = loc.upper()
+    locsplit = loc.split(',')
+    if re.sub('[\s+]', '', locsplit[1]) in states:
+          statecity = [re.sub('[\s+]', '', locsplit[1]), locsplit[0]]
+    elif re.sub('[\s+]', '', locsplit[0]) in states:
+          statecity = [re.sub('[\s+]', '', locsplit[0]), locsplit[1]]
+  return(statecity)
+
+
+class listener(StreamListener):
+
+    data = []
+    lock = None
+
+    def __init__(self, lock):
+
+        # create lock
+        self.lock = lock
+
+        # init timer for database save
+        self.save_in_database()
+
+        # call __inint__ of super class
+        super().__init__()
+
+    def save_in_database(self):
+
+        # set a timer (1 second)
+        Timer(1, self.save_in_database).start()
+
+        # with lock, if there's data, save in transaction using one bulk query
+        with self.lock:
+            if len(self.data):
+
+                now = datetime.datetime.now()   #comment me out later on
+                if now.hour == 4 and now.minute == 43 and now.second < 2.5:  #run a truncate code at every midnight at server local time. Server is 4 hours ahead of EST/NY
+                    print (now.year, now.month, now.day, now.hour, now.minute, now.second)
+                    # 2015 5 6 8 53 40
+                    # database truncate routine. Check if this slows down the twitter stream.
+                    HM_DAYS_KEEP = 2.5  #max days of history in the database
+                    current_ms_time = time.time()*1000
+                    one_day = 86400 * 1000
+                    del_to = int(current_ms_time - (HM_DAYS_KEEP * one_day))
+                    sql = "DELETE FROM sentiment WHERE unix <  '{}'".format(del_to)
+                    c.execute(sql)
+                    time.sleep(5)
+                    # you will need to vacuum if you ever do a bulk delete.
+                    sql = "VACUUM"
+                    c.execute(sql)
+                    time.sleep(5)
+                    print('vacuumed on ', datetime.datetime.now())
+
+
+                c.execute('BEGIN TRANSACTION')
+                try:
+                    c.executemany("INSERT INTO sentiment (unix, tweet, loc, state_code, city_name, country, followers_count, sentiment) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", self.data)
+                except:
+                    pass
+
+                c.execute('COMMIT')
+                self.data = []
+
+    def on_data(self, data):
+        try:
+            #print(data)
+            data = json.loads(data)
+            # there are records like that:
+            # {'limit': {'track': 14667, 'timestamp_ms': '1520216832822'}}
+            if 'truncated' not in data:
+                #print(data)
+                return True
+            if data['truncated']:
+                tweet = unidecode(data['extended_tweet']['full_text'])
+            else:
+                tweet = unidecode(data['text'])
+            time_ms = data['timestamp_ms']
+
+            loc = (data['user']['location'])
+            if loc is not None:
+                state_code = get_state_city(data["user"]["location"])[0]    #update with cleaned up state code like CA, OH, FL
+                city_name = get_state_city(data["user"]["location"])[1]    #update with long form city name
+            else:
+                state_code = ''
+                city_name = ''
+
+            #update loc with the US state abrev from the location free text. If the state name exists than abbrev into state_code
+            if loc is not None and state_code == '':
+                state_code = update_state(loc, us_state_abbrev)
+                #print('new updated state code is: ', state_code)
+
+
+            if data['place'] is not None:
+                country = data['place']   #update with available place info. Though there is place attribute filled in. Maybe change this with coordinates based in the future.
+            else:
+                country = ''
+
+            followers_count  = (data['user']['followers_count'])
+            vs = analyzer.polarity_scores(tweet)
+            sentiment = vs['compound']
+            #print(time_ms, tweet, sentiment)
+
+            # append to data list (to be saved every 1 second)
+            with self.lock:
+                self.data.append((time_ms, tweet, loc, state_code, city_name, country, followers_count, sentiment))
+
+        except KeyError as e:
+            print('There is a KeyError.')
+            print(str(e))
+        return True
+
+    def on_error(self, status_code):
+        print(status_code, 'There is a twitter api on error code triggred. I will sleep a bit and then restart.')
+        time.sleep(60)
+        return False # To continue listening
+
+
+
+def map_nouns(col):
+    return [word[0] for word in TextBlob(col).tags if word[1] == u'NNP']
+
+# make a counter with blacklist words and empty word with some big value - we'll use it later to filter counter
+stop_words.append('')
+blacklist_counter = Counter(dict(zip(stop_words, [1000000] * len(stop_words))))
+
+# compile a regex for split operations (punctuation list, plus space and new line)
+punctuation = [str(i) for i in string.punctuation]
+split_regex = re.compile("[ \n" + re.escape("".join(punctuation)) + ']')
+
+
+while True:   #infinite while loop to listen Twitter API
+
+    try:
+        auth = OAuthHandler(ckey, csecret)
+        auth.set_access_token(atoken, asecret)
+        twitterStream = Stream(auth, listener(lock))
+        twitterStream.filter(track=["t"], languages=["en"])      # locations=LOCATIONS
+        #twitterStream.filter(stall_warnings=True)
+
+    except Exception as e:
+        print('exception at...', datetime.datetime.now())
+        print(str(e), "There is an exception with twitter stream!")
+        time.sleep(5)
